1179e17d6f8513e7e97d102fb6c4e4ea949c6b72
“Erika <foglzerika@gmail.com>
2017-05-05 00:39:25 -0500

    Agregando el js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
 /*
  * Archivo principal de JS
  */
+var efectoMenu = document.getElementsByClassName('menu');
+[].forEach.call(efectoMenu,function(elemento){
+  elemento.addEventListener("mouseover",cambiaColor);
+  elemento.addEventListener("mouseout",colorOriginal);
+})
+
+function cambiaColor(){
+  this.style.background="#18bc9c";
+}
+function colorOriginal(){
+  this.style.background="#2c3e50";
+}
